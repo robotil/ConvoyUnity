f5bab753adeed193d1decfb587147f37e56175b1
Robil <robil@deepspace>
2018-01-03 10:00:18 +0200

    Add missed files

---
+++ Assets/Scripts/velodyneScripts/velodyne_ICD/include/Tester.h
@@ -0,0 +1,18 @@
+#ifndef TESTER_H
+#define TESTER_H
+
+/**
+* Tester.h
+* Author: Binyamin Appelbaum
+* Date: 13.12.17
+*/
+
+
+class Tester {
+    void TestVLP();
+public:
+    Tester();
+
+};
+
+#endif // TESTER_H
