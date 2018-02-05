be189604336d5dad80b4c827fe6ad24946ee53a1
daniel <daniel.meltz@gmail.com>
2018-02-05 09:56:08 +0200

    working Novatel DGPS physics and ICD

---
+++ Assets/Scripts/NovatelScripts/DgpsWrapper.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+public class DgpsWrapper : IDisposable {
+	const String DLL_LOCATION = "libdgps";
+
+	[DllImport (DLL_LOCATION)]
+	private static extern IntPtr CreateDgpsObject(string portName, int baudRate);
+
+	[DllImport (DLL_LOCATION)]
+	private static extern void DeleteDgpsObject(IntPtr pDgps);
+
+	[DllImport (DLL_LOCATION)]
+	private static extern void RunDgps(IntPtr pDgps);
+
+	[DllImport (DLL_LOCATION)]
+	private static extern void SendDgpsData(IntPtr pDgps);
+
+	[DllImport (DLL_LOCATION)]
+	private static extern void SetPosition(IntPtr pDgps, double latitude, double longitude, double altitude);
+	
+	[DllImport (DLL_LOCATION)]
+	private static extern void SetVelocities(IntPtr pDgps, double latSpeed, double longSpeed, double altAzimuth);
+
+	[DllImport (DLL_LOCATION)]
+	private static extern void SetDgpsTimeStamp(IntPtr pDgps, int timeStamp);
+
+	private IntPtr m_nativeObject;
+
+	public DgpsWrapper(string portName, int baudRate) {
+		this.m_nativeObject = CreateDgpsObject(portName, baudRate);
+	}
+
+	~DgpsWrapper() {Dispose(false);}
+	
+	public void Dispose() { Dispose(true);}
+
+    protected virtual void Dispose(bool bDisposing) {
+        if (this.m_nativeObject != IntPtr.Zero) {
+            //DeleteDgpsObject(this.m_nativeObject);
+             //this.m_nativeObject = IntPtr.Zero;
+        }
+
+        if (bDisposing) {
+            GC.SuppressFinalize(this);
+        }
+    }
+
+	public void Run() {
+		RunDgps(this.m_nativeObject);
+	}
+
+	public void SetPosition(double latitude, double longitude, double altitude) {
+		SetPosition(this.m_nativeObject, latitude, longitude, altitude);
+	}
+
+	public void SetVelocities(double latSpeed, double longSpeed, double altAzimuth) {
+		SetVelocities(this.m_nativeObject, latSpeed, longSpeed, altAzimuth);
+	}
+
+	public void SetTimeStamp(int timeStamp) {
+		SetDgpsTimeStamp(this.m_nativeObject, timeStamp);
+	}
+
+	public void SendData() {
+		SendDgpsData(this.m_nativeObject);
+	}
+}
---
+++ Assets/Scripts/NovatelScripts/Novatel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//         N (z) (Lat)
+//         ^ 
+//         |
+//  W ------------>  E (x) (Lon)
+//         |
+//         |
+//         S
+
+
+public class Novatel : MonoBehaviour {
+
+	DgpsWrapper DGPSinterface;
+
+	public Vector3 LatLonAltPos0;
+    public Vector3 LatLonAltPos, LatLonAltVel; 
+
+	public float SensorPubFreq = 1;
+    Rigidbody rb;
+    Transform myref;
+
+
+	float Re = 6378.1f * 1000; // Earth radius in meters 
+	float R2D = Mathf.Rad2Deg, D2R = Mathf.Deg2Rad;
+
+	// Use this for initialization
+	void Start () {
+        myref = gameObject.transform;
+        rb = GetComponent<Rigidbody>();
+
+		float pubTimeInterval = 1/SensorPubFreq;
+		InvokeRepeating("PosVelPub", 0.0f, pubTimeInterval);
+
+		DGPSinterface = new DgpsWrapper("/dev/ttyUSB0", 115200);
+	}
+	
+	// Update is called once per frame
+	void PosVelPub() {
+		Vector3 pos = myref.position;
+		float dist = Mathf.Sqrt(pos.x*pos.x + pos.z*pos.z);
+		float brng = Mathf.Atan2(pos.x,pos.z);
+		float hight = pos.y; 
+
+		float lat0 = LatLonAltPos0.x;
+		float lon0 = LatLonAltPos0.y;
+		float alt0 = LatLonAltPos0.z;
+
+		float lat = R2D*asin(sin(lat0*D2R)*cos(dist/Re)+cos(lat0*D2R)*sin(dist/Re)*cos(brng));
+		float lon = lon0 + R2D*atan2(sin(brng)*sin(dist/Re)*cos(lat0*D2R),cos(dist/Re)-sin(lat0*D2R)*sin(lat*D2R));
+		float alt = alt0 + hight;
+
+		LatLonAltPos.x = lat;
+		LatLonAltPos.y = lon;
+		LatLonAltPos.z = alt;
+
+		DGPSinterface.SetPosition(LatLonAltPos.x, LatLonAltPos.y, LatLonAltPos.z);
+
+		Vector3 vel = rb.velocity;
+		LatLonAltVel.x = vel.z;
+		LatLonAltVel.y = vel.x;
+		LatLonAltVel.z = vel.y;
+
+		DGPSinterface.SetVelocities(LatLonAltVel.x , LatLonAltVel.y, LatLonAltVel.z);
+
+        double timeStamp = Time.fixedTime * 1000000.0;
+		DGPSinterface.SetTimeStamp((int)timeStamp);
+	}
+
+
+
+
+	float sin(float x) { return Mathf.Sin(x); }	
+	float asin(float x) { return Mathf.Asin(x); }	
+	float cos(float x) { return Mathf.Cos(x); }	
+	float atan2(float y, float x) { return Mathf.Atan2(y,x); }	
+
+}
