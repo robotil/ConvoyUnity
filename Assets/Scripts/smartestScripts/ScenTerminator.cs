0991e98fa6d977e147c89d9c8573d8a563d0e036
robil <robil@cassini>
2018-02-21 19:46:17 +0200

    aoutomatic scen termination

---
+++ Assets/Scripts/smartestScripts/ScenTerminator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScenTerminator : MonoBehaviour {
+
+	
+	public float ScenTimeMax;	
+	public float ScenTimeLeft;
+
+	public Text ScenTimeLeftText;
+	
+	// Use this for initialization
+	void Start () {
+		ScenTimeMax = 15;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ScenTimeLeft = ScenTimeMax - Time.time;
+
+		ScenTimeLeftText.text = "Time Left : " + ScenTimeLeft.ToString("N2"); 
+
+		if (ScenTimeLeft <= 0){
+			Application.Quit();
+			Debug.Log("Aplication Quit");
+		}
+		
+	}
+}
